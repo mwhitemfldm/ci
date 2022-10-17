9d003aeeca8d3377e0dad780c34c7679c89cc011
mwhitemfldm <miawhitefield@gmail.com>
2022-10-17 18:00:37 +0100

    Add unit test for not any

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,6 +13,8 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1, 2, 3), 1)
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
+        self.assertEqual(ci_course.minimum('Jo', "No"), None)
+        self.assertEqual(ci_course.minimum(), None)
 
 
 if __name__ == '__main__':
